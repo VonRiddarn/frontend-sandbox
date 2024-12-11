897c8c76d5858a51909f0ab02a56e61d7ba71c08
VonRiddarn <vonriddarn@gmail.com>
2024-12-11 14:13:07 +0100

    feat: added protected main refernce in the Scene object

--- src/scenemanager/Scene.ts
+++ src/scenemanager/Scene.ts
@@ -4,16 +4,21 @@ type SceneInfo = {
 	cssPath: string;
 };
 
+// TODO: If we get too many scenes to keep track of, maybe add filters to them like:
+// School, Game, Transpiler, Other
+
 class Scene {
 
 	protected _name: string;
 	protected _author: string;
 	protected _cssPath: string;
+	protected _main: HTMLElement | null = null;
 
 	constructor(name: string, author: string, cssPath: string) {
 		this._name = name;
 		this._author = author;
 		this._cssPath = cssPath;
+		this._main = document.querySelector("main");
 	};
 
 	enter = () => {
