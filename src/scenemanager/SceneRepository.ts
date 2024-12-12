9096de1df3d4e7f51fd7e4a851e065c652d96244
VonRiddarn <vonriddarn@gmail.com>
2024-12-12 05:01:35 +0100

    feat: removed pirate transpiler for clarity when working

--- src/scenemanager/SceneRepository.ts
+++ src/scenemanager/SceneRepository.ts
@@ -1,11 +1,8 @@
 import LandingPage from "../scenes/landing-page/landing-page";
-import PirateTranspiler from "../scenes/pirate-transpiler/pirate-transpiler";
 import { Scene } from "./Scene";
 
 const sceneRepository: Scene[] = [
 	new LandingPage("Landing Page", "Timmy Ö", "landing-page"),
-	new PirateTranspiler("Pirate transpiler", "John Doe", "pirate-transpiler"),
-	// More goes here
 ] as const;
 
 export default sceneRepository;
