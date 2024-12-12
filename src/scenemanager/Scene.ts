ae497793a94b88eafeb928671c0821bed0c5fc54
VonRiddarn <vonriddarn@gmail.com>
2024-12-12 06:20:53 +0100

    refactor: made scene a type instead of a class to adhere to a more functional programing aproach

--- src/scenemanager/Scene.ts
+++ src/scenemanager/Scene.ts
@@ -1,51 +1,12 @@
-type SceneInfo = {
-	name: string;
-	author: string;
-	cssPath: string;
-	htmlPath: string;
-	buttonElement: HTMLElement;
+type Scene = {
+	name: string,
+	author: string,
+	cssPath: string,
+	htmlPath: string,
+	buttonElement?: HTMLElement,
+
+	enter: () => void,
+	exit?: () => void,
 };
 
-// TODO: If we get too many scenes to keep track of, maybe add filters to them like:
-// School, Game, Transpiler, Other
-
-class Scene {
-
-	protected _name: string;
-	protected _author: string;
-	protected _cssPath: string;
-	protected _htmlPath: string;
-	protected _main: HTMLElement | null = null;
-	protected _buttonElement: HTMLElement = document.createElement("undefined-element"); // Hack
-
-	constructor(name: string, author: string, internalName: string) {
-		this._name = name;
-		this._author = author;
-		this._cssPath = `/src/scenes/${internalName}/${internalName}.css`;
-		this._htmlPath = `/src/scenes/${internalName}/${internalName}.html`;
-		this._main = document.querySelector("main");
-	};
-
-	initialize = (buttonElement: HTMLElement) => {
-		this._buttonElement = buttonElement;
-	}
-
-	enter = () => {
-
-	};
-
-	exit = () => {
-
-	}
-
-	toInfo = ():SceneInfo => ({
-			name: this._name,
-			author: this._author,
-			cssPath: this._cssPath,
-			htmlPath: this._htmlPath,
-			buttonElement: this._buttonElement
-	});
-};
-
-export { Scene };
-export type { SceneInfo };
+export default Scene;
